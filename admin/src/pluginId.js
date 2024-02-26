b00c83cc89afc30f6d5bccce8aadb598ebc03bfd
MrFlynn1993 <robert.leimer@proton.me>
2024-02-26 19:25:57 +0100

    only navigation?

--- admin/src/pluginId.js
+++ admin/src/pluginId.js
@@ -1,5 +1,6 @@
 const pluginPkg = require('../../package.json');
 
-const pluginId = pluginPkg.name.replace(/^simple-strapi-plugin-/i, '').replace(/-plugin/i, '');
+// const pluginId = pluginPkg.name.replace(/^simple-strapi-plugin-/i, '').replace(/-plugin/i, '');
+const pluginId = pluginPkg.strapi.name
 
 module.exports = pluginId;

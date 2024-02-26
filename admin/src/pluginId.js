87d670c1e413faf4d72ab6956878ef884db7a8c0
MrFlynn1993 <robert.leimer@proton.me>
2024-02-26 19:57:46 +0100

    simple-strapi, tsconfig

--- admin/src/pluginId.js
+++ admin/src/pluginId.js
@@ -1,5 +1,5 @@
 const pluginPkg = require('../../package.json');
 
-const pluginId = pluginPkg.name.replace(/^simple-strapi-plugin-/i, '').replace(/-plugin/i, '');
+const pluginId = pluginPkg.name.replace(/^simple-strapi-/i, '').replace(/-plugin/i, '');
 
 module.exports = pluginId;
